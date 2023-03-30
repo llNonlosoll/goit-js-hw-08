4c69a042f6073d2660316bbef174a16d0748c486
llNonlosoll <wishmasterar@gmail.com>
2023-03-30 22:12:07 +0200

    third done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+
+refs.form.addEventListener('input', throttle(onInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+insertFormValue();
+
+const formValue = {};
+
+function onInput(evt) {
+  formValue[evt.target.name] = evt.target.value;
+  const formValueJSON = JSON.stringify(formValue);
+  localStorage.setItem(STORAGE_KEY, formValueJSON);
+}
+
+function insertFormValue() {
+  const value = localStorage.getItem(STORAGE_KEY);
+  if (value) {
+    const formValueJSON = JSON.parse(value);
+    const { email = '', message = '' } = formValueJSON;
+    refs.input.value = email;
+    refs.textarea.value = message;
+  }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(localStorage.getItem(STORAGE_KEY));
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
