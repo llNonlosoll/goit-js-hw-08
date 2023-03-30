4c9185b4c9841b24e4d9ffe0d060b9c74e4567a0
llNonlosoll <wishmasterar@gmail.com>
2023-03-30 22:46:18 +0200

    second done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,40 @@
 import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', throttle(onPlay, 500));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+function onPlay(data) {
+  const currentTime = JSON.stringify(data);
+  localStorage.setItem(STORAGE_KEY, currentTime);
+}
+
+function insertTime() {
+  const value = localStorage.getItem(STORAGE_KEY);
+  if (value) {
+    const timeValueJSON = JSON.parse(value);
+    const { seconds } = timeValueJSON;
+    return seconds;
+  }
+}
+
+player
+  .setCurrentTime(insertTime())
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
